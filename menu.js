4728df5ee2753db628718beddfefc520aa57bcd6
algbr06 <cntpvnetbook@gmail.com>
2020-11-27 11:48:08 -0300

    primeirobotwpp

---
+++ menu.js
@@ -0,0 +1,3 @@
+module.exports = {
+	menu: "*SEXTA-FEIRA™ \uD83D\uDE0E\u270C*\r\n   \r\n\u27A1\uFE0F !sexta (legenda de foto) = Transforma sua foto em figurinha\r\n\u27A1\uFE0F !comandos = Abre o menu de comandos\r\n _caso algum comando não seja respondido de primeira, tente novamente"
+}
